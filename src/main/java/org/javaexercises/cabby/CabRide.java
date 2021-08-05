c473f753682980124fd8e94c00e14882f80a829d
pratyushs93 <pratyushsoni93@outlook.com>
2021-08-05 14:16:05 +0530

    calculate fare based on the wait minutes and minimum fare functionality

--- src/main/java/org/javaexercises/cabby/CabRide.java
+++ src/main/java/org/javaexercises/cabby/CabRide.java
@@ -3,16 +3,19 @@ package org.javaexercises.cabby;
 public class CabRide {
     private static final int FARE_PER_KM = 10;
     private static final int FARE_PER_WAIT_MIN = 2;
-
+    private static final int Min_Fare = 40;
+    private static int actualFare;
     private final int distanceTravelled;
-    private final int waitMinute;
+    private final int waitTimeInMinutes;
 
-    public CabRide(int distanceTravelledInKm, int waitMinute) {
-        this.waitMinute = waitMinute;
+    public CabRide(int distanceTravelledInKm, int waitTimeInMinute) {
+        this.waitTimeInMinutes = waitTimeInMinute;
         this.distanceTravelled = distanceTravelledInKm;
     }
 
     public int fare() {
-        return ((distanceTravelled * FARE_PER_KM) + (waitMinute * FARE_PER_WAIT_MIN));
+        actualFare = ((distanceTravelled * FARE_PER_KM) + (waitTimeInMinutes * FARE_PER_WAIT_MIN));
+        return ((actualFare < Min_Fare ? Min_Fare : actualFare));
     }
 }
+
--- src/test/java/org/javaexercises/cabby/CabRideTest.java
+++ src/test/java/org/javaexercises/cabby/CabRideTest.java
@@ -6,20 +6,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CabRideTest {
     @Test
-    public void cabFareShouldBe_34_For_3_KmTravelled() {
+    public void cabFareShouldBe_40_For_3_KmTravelled() {
         CabRide cabRide = new CabRide(3,2);
-        assertEquals(34, cabRide.fare());
+        assertEquals(40, cabRide.fare());
     }
 
     @Test
-    public void cabFareShouldBe_26_For_2_KmTravelled() {
+    public void cabFareShouldBe_40_For_2_KmTravelled() {
         CabRide cabRide = new CabRide(2,3);
-        assertEquals(26, cabRide.fare());
+        assertEquals(40, cabRide.fare());
     }
 
     @Test
-    public void cabFareShouldBe_24_For_2_Minutes_Wait_Period() {
+    public void cabFareShouldBe_40_For_2_Minutes_Wait_Period() {
         CabRide cabRide = new CabRide(2, 2);
-        assertEquals(24, cabRide.fare());
+        assertEquals(40, cabRide.fare());
+    }
+
+    @Test
+    public void cabFareShouldBe_40_For_0_Minutes_Wait_Period_And_0_KmTravelled() {
+        CabRide cabRide = new CabRide(0, 0);
+        assertEquals(40, cabRide.fare());
+    }
+
+    @Test
+    public void cabFareShouldBe_54_For_0_Minutes_Wait_Period_And_0_KmTravelled() {
+        CabRide cabRide = new CabRide(5, 2);
+        assertEquals(54, cabRide.fare());
     }
 }
